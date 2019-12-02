0e8e7e8e7c4adf780448c6ec3a9bbd47ef192ec6
Matthias Dellweg <2500@gmx.de>
2019-12-03 00:08:35 +0100

    Add check_mode tests and test docs

--- tests/test_playbooks.py
+++ tests/test_playbooks.py
@@ -17,7 +17,7 @@ if sys.version_info[0] == 2:
             os.environ.pop(envvar)
 
 
-def run_playbook_vcr(tmpdir, test_name, extra_vars=None, record=False):
+def run_playbook_vcr(tmpdir, test_name, extra_vars=None, record=False, check_mode=False):
     playbook = test_name + '.yaml'
     if extra_vars is None:
         extra_vars = {}
@@ -37,18 +37,34 @@ def run_playbook_vcr(tmpdir, test_name, extra_vars=None, record=False):
     params_file = tmpdir.join('test_params_{}.json'.format(test_name))
     params_file.write(json.dumps(test_params))
     os.environ['PAM_TEST_VCR_PARAMS_FILE'] = params_file.strpath
-    return run_playbook(playbook, extra_vars=extra_vars, limit=limit)
+    return run_playbook(playbook, extra_vars=extra_vars, limit=limit, check_mode=check_mode)
 
 
-def run_playbook(playbook, extra_vars=None, limit=None):
+def run_playbook(playbook, extra_vars=None, limit=None, check_mode=False):
     # Assemble parameters for playbook call
     os.environ['ANSIBLE_CONFIG'] = os.path.join(os.getcwd(), 'ansible.cfg')
-    playbook_path = os.path.join(os.getcwd(), 'tests', 'playbooks', playbook)
-    inventory_path = os.path.join(os.getcwd(), 'tests', 'inventory', 'hosts')
-    return ansible_runner.run(extravars=extra_vars, playbook=playbook_path, limit=limit, verbosity=4, inventory=inventory_path)
+    kwargs = {}
+    kwargs['playbook'] = os.path.join(os.getcwd(), 'tests', 'playbooks', playbook)
+    kwargs['inventory'] = os.path.join(os.getcwd(), 'tests', 'inventory', 'hosts')
+    kwargs['verbosity'] = 4
+    if extra_vars:
+        kwargs['extravars'] = extra_vars
+    if limit:
+        kwargs['limit'] = limit
+    if check_mode:
+        kwargs['cmdline'] = "--check"
+    return ansible_runner.run(**kwargs)
 
 
 @pytest.mark.parametrize('test_name', TEST_NAMES)
 def test_playbook(tmpdir, test_name, record):
     run = run_playbook_vcr(tmpdir, test_name, record=record)
+    assert run.rc == 0
+
+
+@pytest.mark.parametrize('test_name', TEST_NAMES)
+def test_check_mode(tmpdir, test_name):
+    # if test_name == 'not_working_one':
+    #     pytest.skip("TODO: Fix check_mode test for not_working_one.")
+    run = run_playbook_vcr(tmpdir, test_name, check_mode=True)
     assert run.rc == 0
