46381a2a2215c6de70514801f6c194710b1b55dc
Matthias Dellweg <2500@gmx.de>
2023-04-20 20:48:51 +0200

    Add api_call module

---
+++ plugins/modules/api_call.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# copyright (c) 2019, Matthias Dellweg
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+DOCUMENTATION = r"""
+---
+module: api_call
+short_description: TBD
+description:
+  - "TBD"
+options:
+  operation_id:
+    description: "ID of the openapi operation to perform."
+    type: str
+    required: true
+  parameters:
+    description: "Dictionary of (path, query and cookie) parameters to send along with the request."
+    type: dict
+    required: false
+  body:
+    description: "JSON representation of the body to send in the request (only POST, PUT and PATCH requests.)"
+    type: dict
+    required: false
+extends_documentation_fragment:
+  - pulp.squeezer.pulp
+author:
+  - Matthias Dellweg (@mdellweg)
+"""
+
+EXAMPLES = r"""
+- name: Read status from pulp api server
+  pulp.squeezer.api_call:
+    pulp_url: https://pulp.example.org
+    username: admin
+    password: password
+  register: pulp_result
+- name: Report pulp status
+  debug:
+    var: pulp_result.response
+"""
+
+RETURN = r"""
+  response:
+    description: Pulp api response
+    type: dict
+    returned: always
+"""
+
+
+from ansible_collections.pulp.squeezer.plugins.module_utils.pulp_glue import PulpAnsibleModule
+
+
+def main():
+    with PulpAnsibleModule(
+        argument_spec=dict(
+            operation_id=dict(required=True),
+            parameters=dict(type="dict"),
+            body=dict(type="dict"),
+        )
+    ) as module:
+        operation_id = module.params["operation_id"]
+        parameters = module.params["parameters"]
+        body = module.params["body"]
+        if module.pulp_ctx.api.operations[operation_id][0].upper() not in ["GET", "HEAD"]:
+            module.set_changed()
+            if module.check_mode:
+                module.set_result("response", None)
+                return
+        response = module.pulp_ctx.call(operation_id, parameters=parameters, body=body)
+        module.set_result("response", response)
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_playbooks.py
+++ tests/test_playbooks.py
@@ -12,14 +12,14 @@ IGNORED_WARNINGS = []
 
 # Clean environment from anything that could cause encoding problems
 if sys.version_info[0] == 2:
-    for envvar in os.environ.keys():
+    for envvar in os.environ:
         try:
             os.environ[envvar] = os.environ[envvar].decode("utf-8").encode("ascii", "ignore")
         except UnicodeError:
             os.environ.pop(envvar)
 
 
-def run_playbook_vcr(tmpdir, test_name, extra_vars=None, record=False, check_mode=False):
+def run_playbook_vcr(tmp_path, test_name, extra_vars=None, record=False, check_mode=False):
     if extra_vars is None:
         extra_vars = {}
     limit = None
@@ -40,16 +40,18 @@ def run_playbook_vcr(tmpdir, test_name, extra_vars=None, record=False, check_mod
         "record_mode": record_mode,
         "check_mode": check_mode,
     }
-    params_file = tmpdir.join("test_params_{}.json".format(test_name))
-    params_file.write(json.dumps(test_params))
-    os.environ["PAM_TEST_VCR_PARAMS_FILE"] = params_file.strpath
-    os.environ["XDG_CACHE_HOME"] = tmpdir.join("cache").strpath
-    return run_playbook(test_name, extra_vars=extra_vars, limit=limit, check_mode=check_mode)
+    params_file = tmp_path / f"test_params_{test_name}.json"
+    params_file.write_text(json.dumps(test_params))
+    os.environ["PAM_TEST_VCR_PARAMS_FILE"] = str(params_file)
+    return run_playbook(
+        tmp_path, test_name, extra_vars=extra_vars, limit=limit, check_mode=check_mode
+    )
 
 
-def run_playbook(test_name, extra_vars=None, limit=None, check_mode=False):
+def run_playbook(tmp_path, test_name, extra_vars=None, limit=None, check_mode=False):
     # Assemble parameters for playbook call
     playbook = test_name + ".yaml"
+    os.environ["XDG_CACHE_HOME"] = str(tmp_path / "cache")
     os.environ["ANSIBLE_CONFIG"] = os.path.join(os.getcwd(), "ansible.cfg")
     kwargs = {}
     kwargs["playbook"] = os.path.join(os.getcwd(), "tests", "playbooks", playbook)
@@ -65,12 +67,12 @@ def run_playbook(test_name, extra_vars=None, limit=None, check_mode=False):
 
 
 @pytest.mark.parametrize("test_name", TEST_NAMES)
-def test_playbook(tmpdir, test_name, vcrmode, pulp_container_log):
+def test_playbook(tmp_path, test_name, vcrmode, pulp_container_log):
     if vcrmode == "live":
-        run = run_playbook(test_name)
+        run = run_playbook(tmp_path, test_name)
     else:
         record = vcrmode == "record"
-        run = run_playbook_vcr(tmpdir, test_name, record=record)
+        run = run_playbook_vcr(tmp_path, test_name, record=record)
     assert run.rc == 0
 
     for event in run.events:
@@ -83,9 +85,9 @@ def test_playbook(tmpdir, test_name, vcrmode, pulp_container_log):
 
 
 @pytest.mark.parametrize("test_name", TEST_NAMES)
-def test_check_mode(tmpdir, test_name, vcrmode):
+def test_check_mode(tmp_path, test_name, vcrmode):
     assert vcrmode == "replay", "Check-mode tests only work in replay."
     # if test_name == 'not_working_one':
     #     pytest.skip("TODO: Fix check_mode test for not_working_one.")
-    run = run_playbook_vcr(tmpdir, test_name, check_mode=True)
+    run = run_playbook_vcr(tmp_path, test_name, check_mode=True)
     assert run.rc == 0
